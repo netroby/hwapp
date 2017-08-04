5bb28c2027bf7867e5ca2faae13ee4a80a1d7178
huzhifeng <huzhifeng@douyu.tv>
2017-08-04 11:05:49 +0800

    Initialized the hello world
    Signed-off-by: huzhifeng <huzhifeng@douyu.tv>

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What your name");
+            if (args == null || args.Length == 0) 
+            {
+                throw new ApplicationException("Specifiy the URL of the resource to retrieve");
+            }
+
+            WebClient client = new WebClient();
+            
+            Stream data = client.OpenRead(args[0]);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            Console.WriteLine(s);
+            data.Close();
+            reader.Close();
+        }
+    }
+}
